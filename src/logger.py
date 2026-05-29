8371d768cdc32625751b42a56aff1e57eaf62ea5
gouravsaini1308 <gouravsaini1308@gmail.com>
2026-05-29 11:53:38 +0530

    logging and exception

--- src/logger.py
+++ src/logger.py
@@ -1,34 +1,17 @@
-'''
-## Logging means:
-“Recording important events, messages, errors, and program activity 
-while a program is running.”
-
-## It helps developers:
--track program execution
--debug errors
--monitor applications
-
-## Without Logging
-If a project crashes: Something went wrong
-You may not know:
--where
--why
--when
-'''
-
-
 import logging
 import os
 from datetime import datetime
 
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path = os.path.join(os.getcwd(),"logs",LOG_FILE)
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
 os.makedirs(logs_path,exist_ok=True)
 
-LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
 )
+
+
